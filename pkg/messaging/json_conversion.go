64d057f87017b431dacab81169cbd6f17b543045
Cobalt0s <constantin.y.koval@gmail.com>
2021-10-24 02:07:35 -0700

    save

--- pkg/messaging/json_conversion.go
+++ pkg/messaging/json_conversion.go
@@ -5,13 +5,7 @@ import (
 	"fmt"
 )
 
-type Convertable struct {}
-
-func (n Convertable) ToJSON() (string, error) {
-	return convertToJson(n)
-}
-
-func convertToJson(data interface{}) (string, error) {
+func ConvertToJson(data interface{}) (string, error) {
 	result, err := json.Marshal(data)
 	if err != nil {
 		return "", fmt.Errorf("failed to convert %T to json", data)
