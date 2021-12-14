1e0c7dabbc597087599513929a8734f8acb18ec9
Cobalt0s <constantin.y.koval@gmail.com>
2021-12-13 22:16:42 -0800

    Add CarryOn to Baggage

--- messaging/contextual.go
+++ messaging/contextual.go
@@ -5,5 +5,14 @@ import "github.com/google/uuid"
 type Baggage struct {
 	UserID     uuid.UUID  `json:"userId"`
 	SentAt     CustomTime `json:"sentAt"`
+	CarryOn
+}
+
+type CarryOn struct {
 	ReceiverID uuid.UUID  `json:"receiverId"`
+}
+
+func (b Baggage) WithCarryOn(carryOn CarryOn) Baggage {
+	b.CarryOn = carryOn
+	return b
 }
