89f3f6e92fdfee53857f3df93c29a2d3c3d5f4d0
Cobalt0s <constantin.y.koval@gmail.com>
2021-12-14 19:15:06 -0800

    Make all the fields in baggage as optional

--- messaging/contextual.go
+++ messaging/contextual.go
@@ -3,13 +3,13 @@ package messaging
 import "github.com/google/uuid"
 
 type Baggage struct {
-	UserID     uuid.UUID  `json:"userId"`
-	SentAt     CustomTime `json:"sentAt"`
+	UserID     *uuid.UUID  `json:"userId,omitempty"`
+	SentAt     *CustomTime `json:"sentAt,omitempty"`
 	CarryOn
 }
 
 type CarryOn struct {
-	ReceiverID uuid.UUID  `json:"receiverId"`
+	ReceiverID *uuid.UUID  `json:"receiverId,omitempty"`
 }
 
 func (b Baggage) WithCarryOn(carryOn CarryOn) Baggage {
