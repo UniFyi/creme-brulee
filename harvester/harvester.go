a3a1f33b4e0576e2ff01f42ee97ab6fc508fcd7e
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2022-01-15 16:33:33 +0200

    Update Harvester prompt to use env (#1)

--- harvester/harvester.go
+++ harvester/harvester.go
@@ -2,9 +2,18 @@ package harvester
 
 import (
 	"context"
+	"fmt"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/ctxlogrus"
+	"github.com/unifyi/creme-brulee/config"
 	"github.com/unifyi/creme-brulee/gintonic"
 )
 
 func Prompt(ctx context.Context) {
-	_, _ = gintonic.MakeRequest(ctx, "POST", "http://localhost:4000/prompt", nil)
+	envHarvesterHost, err := config.GetEnv("HARVESTER_HOST")
+	if err != nil {
+		log := ctxlogrus.Extract(ctx)
+		log.Warn("Harvester prompt failed, missing HARVESTER_HOST env var")
+		return
+	}
+	_, _ = gintonic.MakeRequest(ctx, "POST", fmt.Sprintf("%v/prompt", envHarvesterHost), nil)
 }
