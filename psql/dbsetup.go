b64e19011006ae31237464f4e4fd78070671c9e0
Cobalt0s <constantin.y.koval@gmail.com>
2022-01-27 01:02:46 -0800

    Fix DB logging

--- psql/dbsetup.go
+++ psql/dbsetup.go
@@ -7,19 +7,24 @@ import (
 	"github.com/unifyi/creme-brulee/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func NewGORM(ctx context.Context, baseConf *config.BaseConfig, psqlConf *config.PsqlConfig) *gorm.DB {
 	log := ctxlogrus.Extract(ctx)
 
+	dbLogLevel := logger.Silent
+	if baseConf.LogLevel.String() == logrus.DebugLevel.String() {
+		dbLogLevel = logger.Info
+	}
+
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN: psqlConf.GetDataSourcePSQL().String(),
-	}), &gorm.Config{})
+	}), &gorm.Config{
+		Logger: logger.Default.LogMode(dbLogLevel),
+	})
 	if err != nil {
 		log.Fatal(err)
-	}
-	if baseConf.LogLevel.String() == logrus.DebugLevel.String() {
-		db = db.Debug()
 	}
 
 	return db
