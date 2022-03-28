3091a7b2a44f38778b0c46114191351acc09c446
Cobalt0s <constantin.y.koval@gmail.com>
2022-03-28 09:13:19 -0700

    save

--- messaging/contextual.go
+++ messaging/contextual.go
@@ -3,13 +3,14 @@ package messaging
 import "github.com/google/uuid"
 
 type Baggage struct {
-	UserID     *uuid.UUID    `json:"userId,omitempty"`
-	SentAt     *TimeNano3339 `json:"sentAt,omitempty"`
+	UserID *uuid.UUID    `json:"userId,omitempty"`
+	SentAt *TimeNano3339 `json:"sentAt,omitempty"`
 	CarryOn
 }
 
 type CarryOn struct {
-	ReceiverID *uuid.UUID  `json:"receiverId,omitempty"`
+	ReceiverID  *uuid.UUID  `json:"receiverId,omitempty"`
+	ReceiverIDs *[]uuid.UUID `json:"receiverIds,omitempty"`
 }
 
 func (c CarryOn) ToOutgoingBaggage() Baggage {
@@ -18,11 +19,24 @@ func (c CarryOn) ToOutgoingBaggage() Baggage {
 
 func (b Baggage) WithCarryOn(carryOn CarryOn) Baggage {
 
+	var receiverID *uuid.UUID
+	var receiverIDs *[]uuid.UUID
+
 	// perform copy
-	receiverID := *carryOn.ReceiverID
+	if carryOn.ReceiverID != nil {
+		receiverID = &(*carryOn.ReceiverID)
+		//receiverIDs = &[]uuid.UUID{*carryOn.ReceiverID} // TODO this can be useful for deprecating receiverID field
+	} else {
+		if len(carryOn.ReceiverID) != 0 {
+			r := make([]uuid.UUID, len(*carryOn.ReceiverIDs))
+			copy(r, *carryOn.ReceiverIDs)
+			receiverIDs = &r
+		}
+	}
 
 	b.CarryOn = CarryOn{
-		ReceiverID: &receiverID,
+		ReceiverID:  receiverID,
+		ReceiverIDs: receiverIDs,
 	}
 	return b
 }
